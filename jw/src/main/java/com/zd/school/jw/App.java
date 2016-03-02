14576563af8ec0d91636b91a4636e056300b82c1
autumnal_wind <autumnal_wind@yeah.net>
2016-03-02 16:48:12 +0800

    工程创建

---
+++ jw/src/main/java/com/zd/school/jw/App.java
@@ -0,0 +1,13 @@
+package com.zd.school.jw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
